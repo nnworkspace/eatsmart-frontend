1b1502bd1e73484f78158eaa6ad3dfe0b2f58e9d
NN <nnworkspace@gmail.com>
2019-07-27 21:31:24 +0200

    add recipe list and detail views, add shopping list view, in one page

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Hello World Recipe',
+      'This is the hello world recipe',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
+    new Recipe('Hello World Recipe',
+      'This is the hello world recipe',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
