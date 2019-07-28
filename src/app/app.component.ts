69afa90403bb2c3414f760c45e7262375631f284
NN <nnworkspace@gmail.com>
2019-07-28 15:34:55 +0200

    split features under two navi items and fix some dropdown issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'eatsmart-frontend';
-  name = 'lemon';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
