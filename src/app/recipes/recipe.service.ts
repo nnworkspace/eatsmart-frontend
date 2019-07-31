571261a0266a9ae5f2553ef5ab3bb930c287b542
nnworkspace <nnworkspace@gmail.com>
2019-07-31 18:13:20 +0200

    display ingredients of a recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,26 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Hello World Recipe',
-      'This is the hello world recipe',
-      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
-    new Recipe('Another Recipe',
-      'This is the hello world recipe',
-      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
+    new Recipe('Shrimps and Tea Leaves',
+      'Delicious shrimps and tea leaves',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+      [
+        new Ingredient('shrimp', 200, 'g'),
+        new Ingredient('tea leaves', 50, 'g')
+      ]),
+    new Recipe('Crabs and Shrimps',
+      'This is crabs and shrimps',
+      'https://upload.wikimedia.org/wikipedia/commons/5/53/Korean_seafood-Ganjang_gejang-01.jpg',
+      [
+        new Ingredient('crabs', 500, 'g'),
+        new Ingredient('soya sauce', 50, 'ml'),
+        new Ingredient('ginger', 50, 'g')
+      ])
   ];
 
   getRecipes() {
