4da57eb7a8ecc134ec77c364f1fa7d8440d33684
nnworkspace <nnworkspace@gmail.com>
2019-07-31 17:33:18 +0200

    use data service in the recipe components

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import {Recipe} from "./recipe.model";
+import {EventEmitter} from "@angular/core";
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('Hello World Recipe',
       'This is the hello world recipe',
