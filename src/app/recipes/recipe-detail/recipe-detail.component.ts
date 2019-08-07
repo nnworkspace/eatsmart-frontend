355b69a575ecf1b7d8885a4d2e2d3aa83ba3ff7f
nnworkspace <nnworkspace@gmail.com>
2019-08-07 23:01:45 +0200

    link edit router of recipe, and clean up some code

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 import {Recipe} from "../recipe.model";
 import {RecipeService} from "../recipe.service";
@@ -14,6 +14,7 @@ export class RecipeDetailComponent implements OnInit {
   id: number;
 
   constructor(private recipeService: RecipeService,
+              private router: Router,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -27,5 +28,12 @@ export class RecipeDetailComponent implements OnInit {
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+
+    // the following works too
+    // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route} );
   }
 }
