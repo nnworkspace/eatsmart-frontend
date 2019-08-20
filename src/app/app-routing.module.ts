d56c0100a2b34ab23ade4f1a38447acd568329fc
nnworkspace <nnworkspace@gmail.com>
2019-08-20 11:52:53 +0200

    add routing module for recipes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,11 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
-import {RecipesResolverService} from "./recipes/recipes-resolver.service";
 
 import {AuthComponent} from "./auth/auth.component";
-import {RecipesComponent} from "./recipes/recipes.component";
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
-import {RecipeHomeComponent} from "./recipes/recipe-home/recipe-home.component";
-import {RecipeDetailComponent} from "./recipes/recipe-detail/recipe-detail.component";
-import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
-import {AuthGuard} from "./auth/auth.guard";
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {
-    path: 'recipes',
-    component: RecipesComponent,
-    canActivate: [AuthGuard],
-    children: [
-      {path: '', component: RecipeHomeComponent},
-      {path: 'new', component: RecipeEditComponent},
-      {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
-      {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]}
-    ]},
   {path: 'shopping-list', component: ShoppingListComponent},
   {path: 'auth', component: AuthComponent}
 ];
---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,35 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+
+import {RecipesComponent} from "./recipes.component";
+import {AuthGuard} from "../auth/auth.guard";
+import {RecipeHomeComponent} from "./recipe-home/recipe-home.component";
+import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
+import {RecipeDetailComponent} from "./recipe-detail/recipe-detail.component";
+import {RecipesResolverService} from "./recipes-resolver.service";
+
+const recipesRoutes: Routes = [
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {path: '', component: RecipeHomeComponent},
+      {path: 'new', component: RecipeEditComponent},
+      {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent,
+        resolve: [RecipesResolverService]
+      }
+    ]
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forChild(recipesRoutes)],
+  exports: [RouterModule]
+})
+export class RecipesRoutingModule {
+
+}
