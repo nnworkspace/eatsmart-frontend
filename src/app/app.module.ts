b8487086399c08494bf8ff144bbb543a66872e4f
nnworkspace <nnworkspace@gmail.com>
2019-08-27 22:00:54 +0200

    update runtime and add redux tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from "@angular/common/http";
+import {environment} from "../environments/environment";
 import {StoreModule} from "@ngrx/store";
 import {EffectsModule} from "@ngrx/effects";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import {StoreRouterConnectingModule} from "@ngrx/router-store";
 import * as reduxApp from "./redux/app.reducer";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
@@ -13,6 +16,7 @@ import {AppComponent} from './app.component';
 import {HeaderComponent} from "./header/header.component";
 import {AppRoutingModule} from "./app-routing.module";
 import {AuthEffects} from "./auth/redux/auth.effects";
+
 
 @NgModule({
   declarations: [
@@ -25,6 +29,8 @@ import {AuthEffects} from "./auth/redux/auth.effects";
     AppRoutingModule,
     StoreModule.forRoot(reduxApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({logOnly: environment.production}),
+    StoreRouterConnectingModule.forRoot(),
     BrowserAnimationsModule,
     SharedModule,
     CoreModule
