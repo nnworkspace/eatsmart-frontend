29bf4adbabfc75d3c431605cf3adab49f50308dc
nnworkspace <nnworkspace@gmail.com>
2019-09-27 15:19:00 +0200

    ssr is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
+import {isPlatformBrowser} from "@angular/common";
+
 import {Store} from "@ngrx/store";
-
 import * as reduxApp from "./redux/app.reducer";
-import * as AuthActions from './auth/redux/auth.actions';
 
+import * as AuthActions from './auth/redux/auth.actions';
 import {LoggingService} from "./logging.service";
 
 @Component({
@@ -12,11 +13,17 @@ import {LoggingService} from "./logging.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private redux: Store<reduxApp.AppState>, private log: LoggingService) {
+  constructor(
+    private redux: Store<reduxApp.AppState>,
+    private log: LoggingService,
+    @Inject(PLATFORM_ID) private platformId) {
   }
 
   ngOnInit() {
-    this.redux.dispatch(AuthActions.autoLogin());
+    // browser functions, won't work with server-side rendering.
+    if (isPlatformBrowser(this.platformId)) {
+      this.redux.dispatch(AuthActions.autoLogin());
+    }
 
     this.log.printLog('hello from AppComponent ngOnInit');
   }
